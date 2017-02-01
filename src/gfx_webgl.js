05f25adb88bbf0ab8393bf40ecda8d4c470276ac
Andreas Plesch <andreasplesch@users.noreply.github.com>
2017-02-01 11:02:37 -0500

    Update gfx_webgl.js

--- src/gfx_webgl.js
+++ src/gfx_webgl.js
@@ -2970,7 +2970,7 @@ x3dom.gfx_webgl = (function () {
                     //converter[1] = 0;
                     converter[1] = Math.floor(pixelData[index + 1] / 64) * 64; //hi 2 bit now mantissa 2
                     converter[2] = pixelData[index];
-                    converter[3] = pixelData[index + 1] - converter[1];
+                    converter[3] = pixelData[index + 1] - converter[1] + 32; //bias adjustment
 //                    return (new Float32Array(converter.buffer)) * sceneSize;
                     return new Float32Array(converter.buffer);
 //                     return (pixelData[index    ] / 255.0) * denom * denom + // now 3 byte pos.
