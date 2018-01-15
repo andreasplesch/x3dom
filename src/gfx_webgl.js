3b9cb223640a888b9926c30a03e4e709e1a90cef
Andreas Plesch <andreasplesch@users.noreply.github.com>
2018-01-15 08:12:13 -0500

    revert

--- src/gfx_webgl.js
+++ src/gfx_webgl.js
@@ -4204,9 +4204,9 @@ x3dom.gfx_webgl = (function () {
             var face = faces[i];
             gl.bindFramebuffer(gl.FRAMEBUFFER, fbo.fbo);
             gl.bindTexture(gl.TEXTURE_CUBE_MAP, rt._parentNodes[0]._x3domTexture.texture);
-            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+            //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true); // no effect ?
             gl.copyTexImage2D(face, 0, gl.RGBA, 0, 0, fbo.width, fbo.height, 0);
-            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+            //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
             gl.bindTexture(gl.TEXTURE_CUBE_MAP, null);
             gl.bindFramebuffer(gl.FRAMEBUFFER, null);
         }
