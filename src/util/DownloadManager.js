f53c2422a68af09edf1eec7034edc3e71d6baa44
Andreas Plesch <andreasplesch@users.noreply.github.com>
2018-05-23 15:06:00 -0400

    lgtm: boolean never undefined
    presumably intended as: if null or undefined, skip

--- src/util/DownloadManager.js
+++ src/util/DownloadManager.js
@@ -216,7 +216,7 @@ x3dom.DownloadManager = {
       
       //insert requests
       for (k = 0; k < urls.length; ++k) {
-        if (!onloadCallbacks[k] === undefined || !priorities[k] === undefined) {
+        if (!onloadCallbacks[k] || !priorities[k] ) {
           x3dom.debug.logError('DownloadManager: No onload callback and / or priority specified. Ignoring request for \"' + url + '\"');
           continue;
         }
