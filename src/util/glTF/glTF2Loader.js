7c0e5b223bcc68e32715e7e722e0d07263b6bf9d
Andreas Plesch <andreasplesch@users.noreply.github.com>
2018-12-14 17:31:41 -0500

    Update glTF2Loader.js

--- src/util/glTF/glTF2Loader.js
+++ src/util/glTF/glTF2Loader.js
@@ -67,7 +67,7 @@ x3dom.glTF2Loader.prototype._dumpAll = function (x3dScene)
     fetch(this._nameSpace.getURL(this._gltf.buffers[0].uri)).then(
         function (response) {return response.arrayBuffer();}).then(
         function (buffer) {
-            this._glTF.accessors.forEach ( function(accessor, i) {
+            this._gltf.accessors.forEach ( function(accessor, i) {
                 view = accessor.bufferView;
                 byteOffset = accessor.byteOffset + this._glTF.bufferViews[view].byteOffset;
                 components = this._componentsOf(accessor.type);
