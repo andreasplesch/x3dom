ec3d5c9cd58e80db78943b114d2165d1cc0d3c92
Andreas Plesch <andreasplesch@users.noreply.github.com>
2020-10-17 16:32:04 -0400

    lint

--- src/nodes/Scripting/X3DScript.js
+++ src/nodes/Scripting/X3DScript.js
@@ -29,7 +29,7 @@ x3dom.registerNodeType(
             x3dom.nodeTypes.X3DChildNode.superClass.call( this, ctx );
 
             /**
-             * NYI. 
+             * NYI.
              * Once the script has access to a X3D node (via an SFNode or MFNode value either in one of the Script node's fields or passed in as an attribute),
              * the script is able to read the contents of that node's fields. If the Script node's directOutput field is TRUE,
              * the script may also send events directly to any node to which it has access, and may dynamically establish or break routes.
@@ -43,8 +43,8 @@ x3dom.registerNodeType(
              */
             this.addField_SFBool( ctx, "directOutput", false ); //NYI
 
-             /**
-             * NYI. 
+            /**
+             * NYI.
              * If the Script node's mustEvaluate field is FALSE, the browser may delay sending input events to the script until its outputs are needed by the browser. If the mustEvaluate field is TRUE, the browser shall send input events to the script as soon as possible, regardless of whether the outputs are needed. The mustEvaluate field shall be set to TRUE only if the Script node has effects that are not known to the browser (such as sending information across the network). Otherwise, poor performance may result.
              * @var {x3dom.fields.SFBool} mustEvaluate
              * @memberof x3dom.nodeTypes.X3DScript
@@ -53,7 +53,7 @@ x3dom.registerNodeType(
              * @instance
              */
             this.addField_SFBool( ctx, "mustEvaluate", false ); //NYI
-      
+
             /**
              * Contains all fields.x3dom mechanism to allow custom fields.
              * @var {x3dom.fields.MFNode} fields
@@ -105,7 +105,7 @@ x3dom.registerNodeType(
                 {
                     var fieldType = field._vf.type;
                     var fieldName = field._vf.name;
-                    if ( fieldType.endsWith ('ode') ) // cf node
+                    if ( fieldType.endsWith( "ode" ) ) // cf node
                     {
                         var type = x3dom.nodeTypes.X3DNode;
                         //TODO; get type from default value
@@ -113,7 +113,7 @@ x3dom.registerNodeType(
                     }
                     else
                     {
-                        if ( field._vf.value ) this._ctx.xmlNode.setAttribute( fieldName, field._vf.value ); // use dom to set value
+                        if ( field._vf.value ) {this._ctx.xmlNode.setAttribute( fieldName, field._vf.value );} // use dom to set value
                         this[ "addField_" + fieldType ]( this._ctx, fieldName, field._vf.value ); //or default value if none given
                     }
                 }, this );
@@ -126,73 +126,73 @@ x3dom.registerNodeType(
                 {
                     var atype = field._vf.accessType;
                     var fieldName = field._vf.name;
-                    switch (atype.toLowerCase())
+                    switch ( atype.toLowerCase() )
                     {
-                      case "outputonly" :
-                        outputs.push( fieldName );
-                        initValues.push( fieldName );
-                        break;
-                      case "inputoutput" :
-                        outputs.push( fieldName + "_changed" );
-                        inputs.push( "set_" + fieldName );
-                        initValues.push( fieldName );
-                        break;
-                      case "inputonly" :
-                        inputs.push( fieldName );
-                        break;
-                      case "initializeonly" :
-                        initValues.push( fieldName );
-                      default :
-                        x3dom.debug.logWarning( fieldName + " has unrecognized access type: " +  atype );
-                        break;
+                        case "outputonly" :
+                            outputs.push( fieldName );
+                            initValues.push( fieldName );
+                            break;
+                        case "inputoutput" :
+                            outputs.push( fieldName + "_changed" );
+                            inputs.push( "set_" + fieldName );
+                            initValues.push( fieldName );
+                            break;
+                        case "inputonly" :
+                            inputs.push( fieldName );
+                            break;
+                        case "initializeonly" :
+                            initValues.push( fieldName );
+                        default :
+                            x3dom.debug.logWarning( fieldName + " has unrecognized access type: " +  atype );
+                            break;
                     }
-                });
+                } );
                 //wrap source
                 var source = "return function wrapper ( scriptNode ) { \n";
-                var callbacks = ["initialize", "prepareEvents", "eventsProcessed", "shutdown", "getOutputs"].concat( inputs );
-                source += "var " + callbacks.join(",") + ";\n";
-                source += "var " + outputs.join(",") + ";\n";
-                initValues.forEach( function (field)
+                var callbacks = [ "initialize", "prepareEvents", "eventsProcessed", "shutdown", "getOutputs" ].concat( inputs );
+                source += "var " + callbacks.join( "," ) + ";\n";
+                source += "var " + outputs.join( "," ) + ";\n";
+                initValues.forEach( function ( field )
                 {
-                    source += "var " + field + " = scriptNode._vf['" + field +"'];\n";
-                });
-                Object.keys( x3dom.fields ).forEach( function ( field ) 
+                    source += "var " + field + " = scriptNode._vf['" + field + "'];\n";
+                } );
+                Object.keys( x3dom.fields ).forEach( function ( field )
                 {
-                    source += "var " + field + " = x3dom.fields." + field + ";\n";  
-                });
+                    source += "var " + field + " = x3dom.fields." + field + ";\n";
+                } );
                 //TODO add SFRotation, Browser, print ...
                 source += this._source;
                 source += "\n function getOutputs () { \n";
-                source += "return { " + outputs.map( function (c)
+                source += "return { " + outputs.map( function ( c )
                 {
                     return "\n" + c + " : " + c;
-                } ).join(",") + " } };";
-                source += "\n return { " + callbacks.map( function (c)
+                } ).join( "," ) + " } };";
+                source += "\n return { " + callbacks.map( function ( c )
                 {
                     return "\n" + c + " : " + c;
-                } ).join(",") + " } };"
+                } ).join( "," ) + " } };";
                 //make script function
                 this._scriptFunction = new Function( source )();
                 this._callbacks = this._scriptFunction( this ); // pass in this node
                 //run initialize
                 if ( this._callbacks.initialize instanceof Function )
                 {
-                    this._callbacks.initialize( Date.now()/1000 );
+                    this._callbacks.initialize( Date.now() / 1000 );
                 }
             },
 
-            fieldChanged: function (fieldName)
+            fieldChanged : function ( fieldName )
             {
-                if ( this._callbacks[fieldName] instanceof Function )
+                if ( this._callbacks[ fieldName ] instanceof Function )
                 {
                     var preOutputs = this._callbacks.getOutputs();
-                    this._callbacks[fieldName]( this._vf[fieldName], Date.now()/1000 );
+                    this._callbacks[ fieldName ]( this._vf[ fieldName ], Date.now() / 1000 );
                     var postOutputs = this._callbacks.getOutputs();
                     for ( var output in postOutputs )
                     {
                         //if ( postOutputs[output] != preOutputs[output] )
                         {
-                            this.postMessage( output, postOutputs[output] );
+                            this.postMessage( output, postOutputs[ output ] );
                         }
                     }
                 }
@@ -200,41 +200,3 @@ x3dom.registerNodeType(
         }
     )
 );
-/*
-Script : X3DScriptNode {
-  SFNode    [in,out] metadata     NULL  [X3DMetadataObject]
-  MFString  [in,out] url          []    [URI]
-  SFBool    []       directOutput FALSE
-  SFBool    []       mustEvaluate FALSE
-  # And any number of:
-  fieldType [in]     fieldName
-  fieldType [in,out] fieldName    initialValue
-  fieldType [out]    fieldName
-  fieldType []       fieldName    initialValue
-}
-The Script node is used to program behaviour in a scene. Script nodes typically:
-
-signify a change or user action;
-receive events from other nodes;
-contain a program module that performs some computation;
-effect change somewhere else in the scene by sending events.
-Each Script node has associated programming language code, referenced by the url field, that is executed to carry out the Script node's function. That code is referred to as the "script" in the rest of this description. Details on the url field can be found in 9.2.1 URLs.
-
-Browsers are not required to support any specific language. Detailed information on scripting languages is described in 29.2 Concepts. Browsers supporting a scripting language for which a language binding is specified shall adhere to that language binding (see ISO/IEC 19777).
-
-Sometime before a script receives the first event it shall be initialized (any language-dependent or user-defined initialize() is performed). The script is able to receive and process events that are sent to it. Each event that can be received shall be declared in the Script node using the same syntax as is used in a prototype definition:
-
-    inputOnly type name
-The type can be any of the standard X3D fields (as defined in 5 Field type reference). Name shall be an identifier that is unique for this Script node.
-
-The Script node is able to generate events in response to the incoming events. Each event that may be generated shall be declared in the Script node using the following syntax:
-
-    outputOnly type name
-If the Script node's mustEvaluate field is FALSE, the browser may delay sending input events to the script until its outputs are needed by the browser. If the mustEvaluate field is TRUE, the browser shall send input events to the script as soon as possible, regardless of whether the outputs are needed. The mustEvaluate field shall be set to TRUE only if the Script node has effects that are not known to the browser (such as sending information across the network). Otherwise, poor performance may result.
-
-Once the script has access to a X3D node (via an SFNode or MFNode value either in one of the Script node's fields or passed in as an attribute), the script is able to read the contents of that node's fields. If the Script node's directOutput field is TRUE, the script may also send events directly to any node to which it has access, and may dynamically establish or break routes. If directOutput is FALSE (the default), the script may only affect the rest of the world via events sent through its fields. The results are undefined if directOutput is FALSE and the script sends events directly to a node to which it has access.
-
-A script is able to communicate directly with the X3D browser to get information such as the current time and the current world URL. This is strictly defined generally by the SAI services (see Part 2 of ISO/IEC 19775) and by the language bindings of the SAI (see ISO/IEC 19777) for the specific scripting language being used.
-
-The location of the Script node in the scene graph has no affect on its operation.
-*/
