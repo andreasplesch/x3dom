af40c3e9be834f7ed529407217b09d9240a56fdd
Andreas Plesch <andreasplesch@users.noreply.github.com>
2021-05-24 19:50:29 -0400

    Create progressBarPlugin.js

---
+++ test/functional/progressBar/progressBarPlugin.js
@@ -0,0 +1,52 @@
+x3dom.runtime.ready = function()
+{
+    var x3d = document.querySelector('x3d');
+    var progressDiv = x3d.querySelector(".x3dom-progress");
+    var styles
+    progressDiv.children[0].style.display='none';
+    progressDiv.children[1].style.display='none';
+    var barDiv = document.createElement('div');
+    progressDiv.style.transition = 
+        "background-color 10s cubic-bezier(0,2.14,1,-1.14), transform 40s linear";
+    progressDiv.style.backgroundClip = "border-box";
+    barDiv.style.transform = "rotateY(0deg)";
+    barDiv.style.transformStyle = "preserve-3d";
+    progressDiv.style.perspective = "100px";
+
+    //barDiv.setAttribute('class','x3dom-progress');
+    //barDiv.setAttribute('style','display:flex');
+    var max = x3d.querySelectorAll('inline').length; // guess downloads
+    var value = 0;
+    barDiv.innerHTML = '<progress></progress><percent>0%</percent>';
+    progressDiv.appendChild(barDiv);
+    var progress = barDiv.querySelector('progress');
+    progress.style.height = '2em';
+    var progressText = barDiv.querySelector('percent');
+    progressText.style.display = 'block';
+    progressText.style.marginLeft = '1em';
+    progress.max = max;
+    var updateBar = function ( mlist )
+    {
+        mlist.forEach( m =>
+        {
+            if (m.type == "childList")
+            {
+                //console.log(m.target);
+                //value = x3dom.RequestManager.loadedRequests;//+m.target.textContent;
+                value = +m.target.textContent;
+                if ( value > max )
+                {
+                    max = value;
+                    progress.max = max;
+                }
+                progress.value = max - value; // count up
+                progressText.childNodes[0].textContent = Math.round(100 * progress.value/max) + "%";
+                progressDiv.style.backgroundColor = value % 2 ? 'rgba(253,110,55,0.4)' : 'rgba(255,255,255,0.1)';
+                //progressDiv.style.transform = value % 2 ? "rotateY(1441deg)" : "rotateY(0deg)";
+            }
+        });
+    }
+    var o = new MutationObserver( updateBar );
+    o.observe( progressDiv, {subtree: true, childList: true} );
+    //console.log(o);
+}
