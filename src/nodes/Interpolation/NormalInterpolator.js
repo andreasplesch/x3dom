7c9a107be8d70e85ace8800a32038c12b3776b7e
Andreas Plesch <andreasplesch@users.noreply.github.com>
2022-01-18 19:18:45 -0500

    fix NormalInterpolator

--- src/nodes/Interpolation/NormalInterpolator.js
+++ src/nodes/Interpolation/NormalInterpolator.js
@@ -41,6 +41,24 @@ x3dom.registerNodeType(
              * @instance
              */
             this.addField_MFVec3f( ctx, "keyValue", [] );
+
+            if ( ctx && ctx.xmlNode.hasAttribute( "keyValue" ) )
+            {
+                this._vf.keyValue = [];     // FIXME!!!
+
+                var arr = x3dom.fields.MFVec3f.parse( ctx.xmlNode.getAttribute( "keyValue" ) );
+                var key = this._vf.key.length > 0 ? this._vf.key.length : 1;
+                var len = arr.length / key;
+                for ( var i = 0; i < key; i++ )
+                {
+                    var val = new x3dom.fields.MFVec3f();
+                    for ( var j = 0; j < len; j++ )
+                    {
+                        val.push( arr[ i * len + j ] );
+                    }
+                    this._vf.keyValue.push( val );
+                }
+            }
         },
         {
             fieldChanged : function ( fieldName )
@@ -49,7 +67,12 @@ x3dom.registerNodeType(
                 {
                     var value = this.linearInterp( this._vf.set_fraction, function ( a, b, t )
                     {
-                        return a.multiply( 1.0 - t ).add( b.multiply( t ) ).normalize();
+                        var val = new x3dom.fields.MFVec3f();
+                        for ( var i = 0; i < a.length; i++ )
+                        {
+                            val.push(a[i].multiply( 1.0 - t ).add( b[i].multiply( t ) ).normalize());
+                        }
+                        return val
                     } );
 
                     if ( value != undefined && value != this._lastValue )
