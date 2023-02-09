c0b06d786932350f5357d48ef3afbe4cf7f62e6d
Andreas Plesch <andreasplesch@users.noreply.github.com>
2023-02-09 15:59:06 -0500

    dracoGeo

--- src/util/BinaryContainerSetup.js
+++ src/util/BinaryContainerSetup.js
@@ -1405,7 +1405,7 @@ x3dom.BinaryContainerLoader.setupBufferGeo = function ( shape, sp, gl, viewarea,
 
             var bufferIdx = x3dom.BUFFER_IDX[ accessor._vf.bufferType ];
 
-            var bufferViewID = bufferGeo._cf.views.nodes[ view ]._vf.id;
+            var bufferViewID = bufferGeo._cf.views.nodes[ view ]._vf.idx;
 
             shape._webgl.buffers[ bufferIdx ] = x3dom.BinaryContainerLoader.bufferGeoCache[ URL ].buffers[ bufferViewID ];
         }
@@ -1621,7 +1621,7 @@ x3dom.BinaryContainerLoader.setupBufferGeo = function ( shape, sp, gl, viewarea,
             } );
         }
 
-        var dracoDecoderModule, dracoDecoder, dracoBuffer;
+        var dracoDecoderModule, dracoDecoder, dracoGeometry;
 
         x3dom.BinaryContainerLoader.bufferGeoCache[ URL ].promise.then( function ( arraybuffer )
         {
@@ -1632,8 +1632,19 @@ x3dom.BinaryContainerLoader.setupBufferGeo = function ( shape, sp, gl, viewarea,
                     dracoDecoderModule = module;
                     dracoDecoder = new module.Decoder();
                     var view = bufferGeo._cf.views.nodes[0]; // all views the same for draco except dracoId
-                    dracoBuffer = arraybuffer.slice( view._vf.byteOffset,
+                    var dracoArray = new Int8Array( arraybuffer ).slice( view._vf.byteOffset,
                         view._vf.byteOffset + view._vf.byteLength );
+                    var geometryType = dracoDecoder.GetEncodedGeometryType( dracoArray );
+                    if ( geometryType == dracoDecoderModule.TRIANGULAR_MESH )
+                    {
+                        dracoGeometry = new dracoDecoderModule.Mesh();
+                        dracoDecoder.decodeArrayToMesh( dracoArray, dracoArray.length, dracoGeometry );
+                    }
+                    if ( geometryType == dracoDecoderModule.POINT_CLOUD )
+                    {
+                        dracoGeometry = new dracoDecoderModule.PointCloud();
+                        dracoDecoder.decodeArrayToPointCloud( dracoArray, dracoArray.length, dracoGeometry );
+                    }
                 } )
             }
             return arraybuffer
